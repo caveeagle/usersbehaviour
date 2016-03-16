fd099d6b85a11abe5e205f18714f099b1273e433
Cave <caveeagle@gmail.com>
2016-03-17 02:01:29 +0300

    ver 2.0

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function Page02init()
 	}
 }	
 
-function Page02click(page_href)
+function Page02click(x,y)
 {
     var i = document.getElementById("user_id").value;
     
@@ -30,5 +30,51 @@ function Page02click(page_href)
     
     set_cookie("id",i);
     
-    location.href = page_href+i;
+    // ========================================== //
+    
+    var page_href = document.getElementById("button_2_href").href;
+    
+    location.href = page_href+'#'+x+'_'+y+'_'+i;
+}
+
+function Page01click(x,y)
+{
+	var i = getCookie("id");
+	
+	if(!i)
+	{
+	    i = "undef";	
+	}
+    
+    var page_href = document.getElementById("button_1_href").href;
+    
+    location.href = page_href+'#'+x+'_'+y+'_'+i;
+}
+
+function Page03click(x,y)
+{
+	var i = getCookie("id");
+	
+	if(!i)
+	{
+	    i = "undef";	
+	}
+    
+    var page_href = document.getElementById("button_3_href").href;
+    
+    location.href = page_href+'#'+x+'_'+y+'_'+i;
+}
+
+function Page03init()
+{
+   var N = 30;
+   
+   var fname = Math.floor(Math.random()*N)+1;
+   
+   if(fname<10)
+   {
+     fname = "0"+fname;
+   }
+   
+   document.getElementById("gallery_image").src = "./images/A"+fname+".jpg";
 }
