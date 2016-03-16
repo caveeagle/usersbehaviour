739e872e30d6d35f008c175f7a389642400a388e
Cave <caveeagle@gmail.com>
2016-03-17 02:29:06 +0300

    stable version 3.0

--- main.js
+++ main.js
@@ -30,11 +30,13 @@ function Page02click(x,y)
     
     set_cookie("id",i);
     
+    set_cookie("true_parent",1);
+    
     // ========================================== //
     
     var page_href = document.getElementById("button_2_href").href;
     
-    location.href = page_href+'#'+x+'_'+y+'_'+i;
+    location.href = page_href+'?'+x+'_'+y+'_'+i;
 }
 
 function Page01click(x,y)
@@ -48,7 +50,7 @@ function Page01click(x,y)
     
     var page_href = document.getElementById("button_1_href").href;
     
-    location.href = page_href+'#'+x+'_'+y+'_'+i;
+    location.href = page_href+'?'+x+'_'+y+'_'+i;
 }
 
 function Page03click(x,y)
@@ -62,19 +64,30 @@ function Page03click(x,y)
     
     var page_href = document.getElementById("button_3_href").href;
     
-    location.href = page_href+'#'+x+'_'+y+'_'+i;
+    location.href = page_href+'?'+x+'_'+y+'_'+i;
 }
 
 function Page03init()
 {
-   var N = 30;
+   var i = getCookie("true_parent");
    
-   var fname = Math.floor(Math.random()*N)+1;
-   
-   if(fname<10)
+   if(i && i==1)
    {
-     fname = "0"+fname;
+       var N = 30;
+       
+       var fname = Math.floor(Math.random()*N)+1;
+       
+       if(fname<10)
+       {
+         fname = "0"+fname;
+       }
+       
+       document.getElementById("gallery_image").src = "./images/A"+fname+".jpg";
+       
+       set_cookie("true_parent",0);
    }
-   
-   document.getElementById("gallery_image").src = "./images/A"+fname+".jpg";
+   else
+   {
+       set_cookie("true_parent",0);
+   }
 }
